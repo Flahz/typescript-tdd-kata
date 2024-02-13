40f91bfca15e607fe120a5e32060981bb67b5b09
Flahz <mik.lapierre07@gmail.com>
2024-02-12 22:36:43 -0500

    Test tdd kata

--- src/__tests__/fizzbuzz.test.ts
+++ src/__tests__/fizzbuzz.test.ts
@@ -1,33 +1,34 @@
-// import hello from "../hello";
+import FizzBuzz from "../fizzBuzz";
 
-describe('FizzBuzz tdd test', () => {
+const fizzBuzz = new FizzBuzz();
 
-    beforeEach( () => {
-	})
+describe('Tests TDD pour FizzBuzz', () => {
+    it("retourne '1' pour l'entrée 1", () => {
+        expect(fizzBuzz.answerFor(1)).toEqual("1");
+    });
 
-    it('write my first test', () => {
-        expect(1).toEqual(1)
+    it("retourne '2' pour l'entrée 2", () => {
+        expect(fizzBuzz.answerFor(2)).toEqual("2");
+    });
+
+    it("retourne 'Fizz' pour un nombre divisible par 3", () => {
+        expect(fizzBuzz.answerFor(3)).toEqual("Fizz");
+        expect(fizzBuzz.answerFor(6)).toEqual("Fizz");
+    });
+
+    it("retourne 'Buzz' pour un nombre divisible par 5", () => {
+        expect(fizzBuzz.answerFor(5)).toEqual("Buzz");
+        expect(fizzBuzz.answerFor(10)).toEqual("Buzz");
+    });
+
+    it("retourne 'FizzBuzz' pour un nombre divisible à la fois par 3 et 5", () => {
+        expect(fizzBuzz.answerFor(15)).toEqual("FizzBuzz");
+        expect(fizzBuzz.answerFor(30)).toEqual("FizzBuzz");
+    });
+
+    it("retourne le nombre sous forme de chaîne de caractères pour les nombres non divisibles ni par 3 ni par 5", () => {
+        expect(fizzBuzz.answerFor(1)).toEqual("1");
+        expect(fizzBuzz.answerFor(2)).toEqual("2");
+        expect(fizzBuzz.answerFor(4)).toEqual("4");
     });
 });
-
-
-// Given an integer n, return a string array answer (1-indexed) where:
-
-// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-// answer[i] == "Fizz" if i is divisible by 3.
-// answer[i] == "Buzz" if i is divisible by 5.
-// answer[i] == i if non of the above conditions are true.
- 
-
-// Example 1:
-
-// Input: n = 3
-// Output: ["1","2","Fizz"]
-// Example 2:
-
-// Input: n = 5
-// Output: ["1","2","Fizz","4","Buzz"]
-// Example 3:
-
-// Input: n = 15
-// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
---
+++ src/fizzBuzz.ts
@@ -0,0 +1,13 @@
+export default class FizzBuzz {
+    answerFor(number: number): string {
+        if (number % 3 === 0 && number % 5 === 0) {
+            return 'FizzBuzz';
+        } else if (number % 3 === 0) {
+            return 'Fizz';
+        } else if (number % 5 === 0) {
+            return 'Buzz';
+        } else {
+            return number.toString();
+        }
+    }
+}
